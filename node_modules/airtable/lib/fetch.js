1bb7d799481974494ca4a7b5bcabad6096904ac3
Omer Faruk Aydın <65319640+lawyer-omer@users.noreply.github.com>
2021-09-16 15:11:20 +0300

    Delete node_modules directory

--- node_modules/airtable/lib/fetch.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-// istanbul ignore file
-var node_fetch_1 = __importDefault(require("node-fetch"));
-module.exports = typeof window === 'undefined' ? node_fetch_1.default : window.fetch.bind(window);
-//# sourceMappingURL=fetch.js.map
